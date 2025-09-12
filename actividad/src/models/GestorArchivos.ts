0fa82a3869426701b82504389dd9768b59b2ee02
lFonseca117 <laufonseca727@gmail.com>
2025-09-11 22:21:20 -0500

    Update GestorArchivos.ts

--- actividad/src/models/GestorArchivos.ts
+++ actividad/src/models/GestorArchivos.ts
@@ -1,3 +1,6 @@
+// Clase genérica GestorArchivos q maneja una lista de elementos de tipo T.
+// Permite agregar elementos y listar todos los elementos almacenados
+// Cada instancia tiene un nombre q los identifica
 export class GestorArchivos<T> {
   private elementos: T[] = [];
   constructor(public nombre: string) {}
